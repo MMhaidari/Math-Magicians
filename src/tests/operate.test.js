46cf5688e54e5bd4fd15afd44f1e6ccc10403bf9
mohammad mohsen <mohsenhaidari0766@gmail.com>
2023-04-06 18:48:02 +0430

    all operator tests covered now

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -11,8 +11,18 @@ describe('operate', () => {
     expect(operate('0.1', '0.2', '+')).toBe('0.3');
   });
 
+  test('calculates modulo correctly', () => {
+    expect(operate('10', '3', '%')).toBe('1');
+    expect(operate('10', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+
   test('subtracts two numbers', () => {
     expect(operate('10', '5', '-')).toBe('5');
     expect(operate('-0.1', '-0.2', '-')).toBe('0.1');
+  });
+
+  test('multiply two numbers', () => {
+    expect(operate('10', '5', 'x')).toBe('50');
+    expect(operate('20', '15', 'x')).toBe('300');
   });
 });
